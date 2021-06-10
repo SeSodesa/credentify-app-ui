335c4764793a21f9558622952f2c1fc734e994da
Santtu Söderholm <santtu.soderholm@protonmail.com>
2021-06-10 08:23:39 +0300

    Added a title and subtitle to the Skills page, with a placeholder icon

--- pages/skills/index.vue
+++ pages/skills/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="stage">
+    <div class="icon--title mb-2">
+      <img src="~/assets/icons/loader.svg" alt="Skills image" />
+      <h1>Skills</h1>
+    </div>
+    <div class="subtitle">
+      A summary page of your earned skills and achievements.
+    </div>
+  </div>
 </template>
 
 <script></script>
