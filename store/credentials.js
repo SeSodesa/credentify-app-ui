c21f190ab7b2e7c494ca8f49090934a298b795cc
Santtu Söderholm <santtu.soderholm@protonmail.com>
2021-06-15 08:30:44 +0300

    Added store/credentials.js for sharing credentials between pages

---
+++ store/credentials.js
@@ -0,0 +1,26 @@
+/* Stores the credentials of an individual */
+export const state = () => ({
+  credentials: []
+})
+
+export const mutations = {
+  setCredentials(state, credentials) {
+    state.credentials = credentials
+  },
+  forgetCredentials(state) {
+    state.credentials = []
+  }
+}
+
+export const actions = {
+  async getCredentials({ commit }, data) {
+    if (data) {
+      commit('setCredentials', data)
+    } else {
+      const credentials = await this.$axios
+        .get('/profile/credentials')
+        .then((response) => response.data.data)
+      commit('setCredentials', credentials)
+    }
+  }
+}
