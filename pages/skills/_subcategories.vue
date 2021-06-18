21bd06c846257ab85c71febb7c2aa246e2843372
Santtu Söderholm <santtu.soderholm@protonmail.com>
2021-06-18 09:58:19 +0300

    Clicking on a category in the skill chart now shows its subcategories in a similar chart

---
+++ pages/skills/_subcategories.vue
@@ -0,0 +1,23 @@
+<!--
+This page displays graphs related to the subcategories of the credential tags
+-->
+<template>
+  <div class="stage">
+    <div class="icon--title mb-2">
+      <img src="~/assets/icons/bootstrap-puzzle-big.svg" alt="Skills image" />
+      <h1>Subskills</h1>
+    </div>
+    <div class="subtitle">
+      A summary page of your earned skills and achievements.
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    this.$store.commit('nav/setTitle', 'Subskills')
+    this.$store.commit('nav/setBackUrl', '/skills')
+  }
+}
+</script>
+<style></style>
