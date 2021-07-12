62fa9de3f50d696d95b88d59e4c27d984adee64c
Santtu Söderholm <santtu.soderholm@protonmail.com>
2021-07-12 15:26:53 +0300

    Removed clutter related to skill rose page from skill tree page
    Might still need pruning, pun intended.

--- pages/skill_tree/index.vue
+++ pages/skill_tree/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="stage">
-    <div v-if="currentCategoryLevel < maxCategoryLevel">
+    <div v-if="!credential">
       <div class="icon--title mb-2">
         <img src="~/assets/icons/tree_icon.svg" width="45" alt="Skills image" />
         <h1>Skill Tree</h1>
@@ -9,7 +9,7 @@
         A summary page of your earned skills and achievements.
       </div>
     </div>
-    <div v-else-if="currentCategoryLevel === maxCategoryLevel">
+    <div v-else-if="credential">
       <div class="icon--title">
         <img src="~/assets/icons/credentials-big.svg" alt="Credential logo" />
         <h1>Credential details</h1>
@@ -21,37 +21,24 @@
     <div v-else>
       <h1>Oops. Something went wrong…</h1>
     </div>
-    <v-chart
-      v-if="credentialsExist && currentCategoryLevel < maxCategoryLevel"
-      :ref="'category-chart'"
-      class="category-chart"
-      :option="currentChartOption"
-      @click="chartClickAction"
-    />
-    <single-credential-view
-      v-else-if="credentialsExist && currentCategoryLevel === maxCategoryLevel"
-      :credential="credential"
-    />
-    <div v-else>
+    <div v-if="credentials !== {}">
+      Awaiting construction…
+    </div>
+    <div v-else-if="credential === {}">
       No credentials to display…
+    </div>
+    <div v-else>
+      No credentials downloaded…
     </div>
   </div>
 </template>
 
 <script>
-import * as echarts from 'echarts/core'
-import { TitleComponent } from 'echarts/components'
-import { PieChart } from 'echarts/charts'
-import { SVGRenderer } from 'echarts/renderers'
-import VChart from 'vue-echarts'
-import SingleCredentialView from '~/components/SingleCredentialView'
-
-echarts.use([TitleComponent, PieChart, SVGRenderer])
+// TODO: import SingleCredentialView from '~/components/SingleCredentialView'
 
 export default {
   components: {
-    VChart,
-    SingleCredentialView
+    // TODO: SingleCredentialView
   },
   async asyncData({ app, params }) {
     try {
@@ -88,7 +75,6 @@ export default {
   },
   data() {
     return {
-      backUrl: '/skills',
       categoryLevels: Object.freeze({
         1: {
           name: 'Main categories',
@@ -111,62 +97,10 @@ export default {
           color: '#73c0de'
         }
       }),
-      credentialData: {},
-      categoryLevelTitles: {
-        mainCategory: {
-          text: 'Main categories',
-          subtext: '',
-          triggerEvent: false
-        },
-        subCategory(breadcrumb) {
-          return {
-            text: breadcrumb.join(' → '),
-            subtext: '↑ Back up',
-            textStyle: {
-              width: 1000,
-              overflow: 'break'
-            },
-            subtextStyle: {
-              fontSize: 14
-            },
-            triggerEvent: true
-          }
-        }
-      },
-      commonSeriesSettings: {
-        id: 'Skill tree visualization',
-        type: 'pie',
-        radius: [50, '65%'],
-        center: ['50%', '50%'],
-        roseType: 'area',
-        itemStyle: {
-          borderRadius: 8
-        },
-        label: {
-          overflow: 'break',
-          fontSize: 18,
-          formatter(petal) {
-            if ('credential' in petal.data) {
-              const achievement = petal.data.credential.achievement
-              return achievement.name + ': ' + achievement.ectsCreditPoints
-            } else {
-              return petal.name + ': ' + petal.value
-            }
-          },
-          position: 'outer',
-          alignTo: 'edge',
-          edgeDistance: 0,
-          distanceToLabelLine: 10
-        }
-      },
-      breadcrumb: [],
-      optionStackData: []
+      credentialData: null
     }
   },
   computed: {
-    currentBaseColor() {
-      return this.categoryLevels[Object.keys(this.breadcrumb).length].color
-    },
     credential: {
       get() {
         return this.credentialData
@@ -174,12 +108,6 @@ export default {
       set(newValue) {
         this.credentialData = newValue
       }
-    },
-    optionStack() {
-      return this.optionStackData
-    },
-    currentCategoryLevel() {
-      return this.optionStackData.length
     },
     /* Filters data for drawing the summary graph */
     skillTree() {
@@ -256,30 +184,6 @@ export default {
     },
     credentialsExist() {
       return this.credentials.length > 0
-    },
-    maxCategoryLevel() {
-      return Object.keys(this.categoryLevels).length
-    },
-    // Generates the data used in drawing the summary figure
-    mainChartOption() {
-      const colorTints = this.colorTints(
-        this.categoryLevels[1].color,
-        Object.values(this.skillTree).length
-      )
-      return {
-        title: this.categoryLevelTitles.mainCategory,
-        color: colorTints,
-        series: [
-          {
-            name: 'Main categories',
-            ...this.commonSeriesSettings,
-            data: Object.values(this.skillTree)
-          }
-        ]
-      }
-    },
-    currentChartOption() {
-      return this.optionStack[this.optionStack.length - 1]
     }
   },
   created() {
@@ -287,16 +191,9 @@ export default {
     this.$store.commit('nav/setBackUrl', '')
   },
   mounted() {
-    // this.chartOption = this.mainChartOption
-    this.breadcrumb.push('Main categories')
-    this.optionStack.push(this.mainChartOption)
+    // TODO
   },
   methods: {
-    backUp() {
-      this.breadcrumb.pop()
-      this.optionStack.pop()
-      this.credential = {}
-    },
     lowerTag(tag) {
       const whitespaceNormalized = tag
         .trim()
@@ -318,78 +215,6 @@ export default {
     toValidURL(category) {
       const lower = category.trim().toLowerCase()
       return lower.split(/\s+/).join('_')
-    },
-    followCategoryLink(event) {
-      window.location.href = event.data.url
-    },
-    pushChartOption(label, data) {
-      const N = data !== null ? Object.values(data).length : 0
-      const colorTints = this.colorTints(this.currentBaseColor, N)
-      const newOption = {
-        title: this.categoryLevelTitles.subCategory(this.breadcrumb),
-        color: colorTints,
-        series: [
-          {
-            name: label,
-            ...this.commonSeriesSettings,
-            data: data !== null ? Object.values(data) : [null]
-          }
-        ]
-      }
-      this.optionStack.push(newOption)
-    },
-    chartClickAction(chartComponent) {
-      const componentData = chartComponent.data
-      if (chartComponent.componentType === 'title') {
-        this.backUp()
-      } else if (chartComponent.componentType === 'series') {
-        if (this.currentCategoryLevel < this.maxCategoryLevel - 1) {
-          this.breadcrumb.push(componentData.name)
-          this.pushChartOption(
-            this.categoryLevels[this.currentCategoryLevel + 1].name,
-            componentData.children
-          )
-        } else if (
-          this.currentCategoryLevel === this.maxCategoryLevel - 1 &&
-          'credential' in componentData
-        ) {
-          this.breadcrumb.push(componentData.name)
-          this.pushChartOption(componentData.name, null)
-          this.credential = componentData.credential
-        } else {
-          return false
-        }
-      } else {
-        return false
-      }
-      return true
-    },
-    // Generates a gradient of colors from a given darkest base color (0xRGB)
-    // and a number of color tints above it
-    colorTints(darkestColor, nOfTints) {
-      // Which idiot included conflicting linters.
-      // "Unicorn numbers" my ass…
-      const colorMax = 255
-      const darkestAsInt = parseInt(darkestColor.substring(1), 16)
-      if (darkestAsInt < 0x00) {
-        return null
-      }
-      const darkestRed = darkestAsInt >>> 16
-      const darkestGreen = (darkestAsInt << 16) >>> 24
-      const darkestBlue = (darkestAsInt << 24) >>> 24
-      const darkestMax = Math.max(darkestRed, darkestGreen, darkestBlue)
-      const colorDiff = colorMax - darkestMax
-      const colorStep = (1 / nOfTints) * colorDiff
-      const tints = [darkestColor]
-      for (let ii = 1; ii < nOfTints; ii++) {
-        const nextRed = Math.floor(darkestRed + ii * colorStep)
-        const nextGreen = Math.floor(darkestGreen + ii * colorStep)
-        const nextBlue = Math.floor(darkestBlue + ii * colorStep)
-        const nextColorHex =
-          '#' + ((nextRed << 16) + (nextGreen << 8) + nextBlue).toString(16)
-        tints.push(nextColorHex)
-      }
-      return tints
     }
   }
 }
